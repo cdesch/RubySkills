09bce4700668d8fb2cf9d32e00727692f82ae972
Chris Desch <cdesch@gmail.com>
2013-07-26 11:31:58 -0400

    Completed constants, control statements, eceptions, methods, regex and triangle project.

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -15,6 +15,29 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  
+  #check to make sure the triangle is not 0 or negative.
+  if a|b|c <= 0
+      raise TriangleError, "Cannot have a 0 or negative side"
+  end
+   
+  #check the side lengths are not exceeding an extreme amount
+  #Only if all three of these are true, you have a triangle.
+  unless a + b > c && a + c > b && b + c > a
+      raise TriangleError, "Cannot have a side with an extreme length"
+  end
+  
+  if a == b
+    if a == c
+      return :equilateral 
+    else
+         return :isosceles
+   end
+ elsif b == c || a == c
+   return :isosceles
+ else
+   return :scalene
+ end
 end
 
 # Error class used in part 2.  No need to change this code.
